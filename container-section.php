61a986cd709be79df0e767a6696c27af1516db96
Luisnguyen1 <103302594+Luisnguyen1@users.noreply.github.com>
2024-03-18 17:30:26 +0700

    container-section

---
+++ container-section.php
@@ -0,0 +1,15 @@
+<html lang="en">
+<body>
+    <section class="container">
+        <div class = "container-box">
+            <div class = "container-img">
+                <img src="img/icon/user-graduate.svg" alt="user-graduate">
+            </div>
+            <div class = "container-text">
+                <h4>5K</h4>
+                <p>Online Courses</p>
+            </div>
+        </div>
+    </section>
+</body>
+</html>
--- index.php
+++ index.php
@@ -13,10 +13,12 @@
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
-
+    <script src="js/script.js"></script>
 </head>
+
 <body>
     <?php include('header.php'); ?> 
-    <?php include('banner-section.php'); ?> 
+    <?php include('home-section.php'); ?> 
+    <?php include('container-section.php'); ?>
 </body>
 </html>
